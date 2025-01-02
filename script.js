7f9ac523400d61beb352def8dee6fc5e99808672
natalie <wong.lt.natalie@gmail.com>
2025-01-01 22:31:30 -0500

    Created the Etch-a-Sketch grid using JavaScript

---
+++ script.js
@@ -0,0 +1,27 @@
+// VARIABLES
+const container = document.getElementById("container");
+let dimension = 16;
+
+// FUNCTIONS
+
+// creates a singular row of dimension divs
+function createRow() {
+    const row = document.createElement("div");
+    row.setAttribute("id", "row");
+
+    for (let i = 0; i < dimension; ++i) {
+        const pixel = document.createElement("div");
+        row.appendChild(pixel);
+    }
+    container.appendChild(row);
+}
+
+// creates a dimension x dimension grid 
+function createGrid() {
+    for (let i = 0; i < dimension; ++i) {
+        createRow();
+    }
+}
+
+// MAIN
+createGrid();
