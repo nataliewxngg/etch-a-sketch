905d6114cf52a3ac358ad2d57e219d2d4be20e91
nataliewxngg <wong.lt.natalie@gmail.com>
2025-01-02 10:02:58 -0500

    Added a color picker

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // VARIABLES
 const container = document.querySelector("#container");
+const colorPicker = document.querySelector("input");
 let dimension = 16;
 let enableDraw = false;
 
@@ -13,21 +14,21 @@ function createRow() {
     for (let i = 0; i < dimension; ++i) {
         const pixel = document.createElement("div");
 
-        pixel.addEventListener("mousedown", () => {
+        pixel.addEventListener("mousedown", (e) => {
+            e.preventDefault(); // prevents mouse from becoming "block" icon 
             enableDraw = true;
-            pixel.style["background"] = "black";
-            pixel.style["border"] = "1px solid black";
+            pixel.style["background"] = colorPicker.value;
+            pixel.style["border"] = `1px solid ${colorPicker.value}`;
         });
         pixel.addEventListener("mouseup", () => {
             enableDraw = false;
         });
         pixel.addEventListener("mouseover", () => {
             if (enableDraw) {
-                pixel.style["background"] = "black";
-                pixel.style["border"] = "1px solid black";
+                pixel.style["background"] = colorPicker.value;
+                pixel.style["border"] = `1px solid ${colorPicker.value}`;
             }
         });
-
         row.appendChild(pixel);
     }
     container.appendChild(row);
